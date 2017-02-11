a8bff06c4c24c6dc9e5c3b18ecec54114f165173
Sean Chapman <seankchapman@gmail.com>
2017-02-10 22:53:06 -0800

    completed problem 2

--- problem-2.py
+++ problem-2.py
@@ -1,13 +1,22 @@
 #Problem 2 - Even Fibonacci Numbers
 #By considering the terms in the fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms
 
+from math import sqrt
 def main():
-    print(fib(100))
+    #Initialize variables
+    sum = 0
+    i = 1;
 
+    #Between 0-100, check if the i'th term if the fibonacci sequence is even and below 4 million
+    #If it is, add it to the sum
+    for i in range(100):
+        if int(fib(i)) % 2 == 0 and fib(i) <= 4000000:
+            sum = sum + fib(i)
+            
+    #After loop is complete, print sum
+    print(sum)
+
+#Returns the n'th term of the fibonacci sequence
 def fib(n):
-    if n==0:
-        return 0
-    elif n==1:
-        return 1
-    else: return fib(n-1) + fib(n-2)
+    return ((1+sqrt(5))**n-(1-sqrt(5))**n)/(2**n*sqrt(5))
 main()
