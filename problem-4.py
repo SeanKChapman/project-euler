1c75a85ca3ae0e5873523f2ed0e4c105cf281d10
Sean Chapman <seankchapman@gmail.com>
2017-02-15 11:27:28 -0800

    completed promblem 4

--- problem-4.py
+++ problem-4.py
@@ -3,10 +3,29 @@
 #Find the largest palindrome made from the product of two 3-digit numbers.
 
 def main():
-    max = 0
-    for i in range(100,999):
-        for n in range(100,999):
-            if i*n > max:
-                max = i*n
-    print max
+    allPalindromes = []
+    for i in range (100,999):
+        for j in range(i, 999):
+            product = i*j
+            if checkPalindrome(product) == True:
+                allPalindromes.append(product)
+    print max(allPalindromes)
+
+def checkPalindrome(n):
+    digits = []
+    while n != 0:
+        digits.append(n % 10)
+        n  = n // 10
+    isPalindrome = True
+    for i in range(len(digits)/2):
+        if digits[i] != digits[len(digits) - i -1]:
+            isPalindrome = False
+            break
+
+    if isPalindrome == True:
+        #print digits
+        return True
+
+
+
 main()
